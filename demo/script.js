ef03e49dc8cc25a1511492ae0b5dc44c92776bff
Saurabh209 <saurabhhh2613@gmail.com>
2024-12-18 19:48:36 +0530

    new demo model added(which is basically final version

--- demo/script.js
+++ demo/script.js
@@ -1,8 +1,122 @@
+particlesJS('particles-js', {
+    particles:  {
+        "number": {
+          "value": 400,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#fff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 10,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 500,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 2,
+          "direction": "bottom-right",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 480.4073854628725,
+            "rotateY": 720.6110781943087
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "remove"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 0.5
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 4,
+            "duration": 0.3,
+            "opacity": 1,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+});
+
+
+
 let userValue = document.querySelector(".input")
 let mainContainer = document.querySelector(".mainContainer")
 let userName;
 function Main(){
     let promise = fetch(`https://api.github.com/users/${userName}`);
+    
 
     promise.then((response)=>{
         if(response.status !== 200){
@@ -13,6 +127,7 @@ function Main(){
     }).then((responseData)=>{
 
         mainContainer.innerHTML="";
+
 
         const userContainer = document.createElement('div');
         userContainer.classList.add("userContainer");
@@ -32,9 +147,9 @@ function Main(){
         const profile_name = document.createElement('h1');
         profile_name.innerText=(`${responseData.name}`)
         const profile_username = document.createElement('h4');
-        profile_username.innerText=`${responseData.login}`;
+        profile_username.innerText=`@${responseData.login}`;
         const creationDate = document.createElement('h5');
-        creationDate.innerText = `${responseData.created_at}`;
+        creationDate.innerText = `Joined At ${responseData.created_at}`;
         const bio = document.createElement('p');
         bio.innerText=`${responseData.bio}`
 
@@ -86,90 +201,23 @@ function Main(){
                 repoHolder.appendChild(rep0);
                 repositories.append(repoHolder);
             }
-
-        })
-
-
-
-       
-    })
+    });
+})
 }
+
 
 function onClick(){
     userName = userValue.value.trim();
    if(userName==""){
     userName="Saurabh209"
+    console.log('DOne');
     Main();
    }else{
     Main();
+    console.log('done dpn');
    }
 }
 
-
-
-
 const button = document.querySelector(".button");
 
-button.addEventListener("click",onClick)
-
-
-particlesJS('particles-js', {
-    particles: {
-        number: {
-            value: 80,
-            density: {
-                enable: true,
-                value_area: 800
-            }
-        },
-        color: {
-            value: '#ffffff'
-        },
-        shape: {
-            type: 'circle',
-            stroke: {
-                width: 0,
-                color: '#000000'
-            }
-        },
-        opacity: {
-            value: 0.5,
-            random: false
-        },
-        size: {
-            value: 3,
-            random: true
-        },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: '#ffffff',
-            opacity: 0.4,
-            width: 1
-        },
-        move: {
-            enable: true,
-            speed: 6,
-            direction: 'none',
-            random: false,
-            straight: false,
-            out_mode: 'out',
-            bounce: false
-        }
-    },
-    interactivity: {
-        detect_on: 'canvas',
-        events: {
-            onhover: {
-                enable: true,
-                mode: 'repulse'
-            },
-            onclick: {
-                enable: true,
-                mode: 'push'
-            },
-            resize: true
-        }
-    },
-    retina_detect: true
-});
+button.addEventListener("click",onClick);
